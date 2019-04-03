39c808daab570b7fcc83a1dac461168b04cd0c94
Alexander Snov <snov.contact@gmail.com>
2019-04-03 16:58:35 +0700

    Add logout functionality

--- src/modules/dashboard/components/Header/Header.tsx
+++ src/modules/dashboard/components/Header/Header.tsx
@@ -4,20 +4,27 @@ import { connect } from "react-redux";
 import { Store } from "../../../../store";
 
 import { activeThemeSelector } from "../../../settings/selectors";
+import { logout } from "../../../authentication/actionsApi";
 
 import { HeaderIcon } from "./HeaderIcon";
+import { LogoutIcon } from "./LogoutIcon";
 import styles from "./Header.module.scss";
 
 const mapStateToProps = (state: Store) => ({
   theme: activeThemeSelector(state)
 });
 
+const mapDispatchToProps = {
+  logout
+};
+
 type ModelProps = ReturnType<typeof mapStateToProps>;
-type InjectedProps = ModelProps;
+type DispatchProps = typeof mapDispatchToProps;
+type InjectedProps = ModelProps & DispatchProps;
 
 export const HeaderUnconnected: React.FC<{}> = props => {
   const { children } = props;
-  const { theme } = props as InjectedProps;
+  const { theme, logout } = props as InjectedProps;
   return (
     <div className={`${styles.wrapper} secondary-text`}>
       <span className={styles.icon}>
@@ -28,8 +35,22 @@ export const HeaderUnconnected: React.FC<{}> = props => {
         />
       </span>
       <h3>{children}</h3>
+      <span
+        className={`${styles.icon} ${styles["logout-icon"]}`}
+        onClick={logout}
+        title="Выйти"
+      >
+        <LogoutIcon
+          width={40}
+          height={40}
+          fill={theme === "light" ? "black" : "white"}
+        />
+      </span>
     </div>
   );
 };
 
-export const Header = connect(mapStateToProps)(HeaderUnconnected);
+export const Header = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(HeaderUnconnected);
